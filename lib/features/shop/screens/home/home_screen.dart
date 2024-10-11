b8a7918fb09377eca5b6d20a4a74b085ca364880
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2024-10-11 06:45:09 -0700

    Custome Shapes

--- lib/features/shop/screens/home/home_screen.dart
+++ lib/features/shop/screens/home/home_screen.dart
@@ -1,10 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:moderne_app/common/widgets/custom_shapes/circular_container.dart';
+import 'package:moderne_app/common/widgets/custom_shapes/curved_edges.dart';
+import 'package:moderne_app/util/constants/colors.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+          child: Column(children: [
+        ClipPath(
+          clipper: CustomCurvedEdges(),
+          child: Container(
+            color: TColors.primary,
+            padding: EdgeInsets.all(0),
+            child: SizedBox(
+              height: 400,
+              child: Stack(
+                children: [
+                  Positioned(
+                      top: 100,
+                      right: -300,
+                      child: CircularContainer(
+                        backgroundColor: TColors.white.withOpacity(0.1),
+                      )),
+                  Positioned(
+                      top: -150,
+                      right: -250,
+                      child: CircularContainer(
+                        backgroundColor: TColors.white.withOpacity(0.1),
+                      )),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ])),
+    );
   }
 }
